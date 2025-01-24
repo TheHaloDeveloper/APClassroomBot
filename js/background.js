3d9a1cec61a0b0bd98d26f59610fa3a5797b8011
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-01-24 11:29:40 -0800

    all answers into array, lettered

--- js/background.js
+++ js/background.js
@@ -4,9 +4,19 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
             chrome.scripting.executeScript({
                 target: { tabId: tabId },
                 function: () => {
+                    let all = "ABCDE";
+
                     setTimeout(() => {
-                        const element = document.getElementsByClassName("lrn_stimulus_content")[0].textContent;
-                        console.log(element);
+                        let question = document.getElementsByClassName("lrn_stimulus_content")[0].textContent;
+                        let answers = [];
+
+                        for (let answer of document.getElementsByClassName("lrn_mcqgroup")[0].children) {
+                            let text = answer.children[1].children[0].children[0].children[0].children[0].children[0].children[0].textContent;
+                            answers.push(`${all[answers.length]}. ${text}`);
+                        }
+
+                        console.log(question);
+                        console.log(answers);
                     }, 5000);
                 },
             });

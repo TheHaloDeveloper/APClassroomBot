4a4c74a348dc94792c53f058c810bc44dc31df0c
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-01-24 11:17:55 -0800

    get question

--- js/background.js
+++ js/background.js
@@ -1,9 +1,15 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.url && tab.url.includes("collegeboard.org")) {
         if (tab.url.includes("assessments/assignments")) {
-            setTimeout(function() {
-                console.log("GO!");
-            }, 5000);
+            chrome.scripting.executeScript({
+                target: { tabId: tabId },
+                function: () => {
+                    setTimeout(() => {
+                        const element = document.getElementsByClassName("lrn_stimulus_content")[0].textContent;
+                        console.log(element);
+                    }, 5000);
+                },
+            });
         }
     }
 });

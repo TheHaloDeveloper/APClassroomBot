6589bd058f42c5eacd51a22d1c651db0ebdcb67b
Halo <68120801+TheHaloDeveloper@users.noreply.github.com>
2025-01-25 01:09:03 -0800

    backtick instead of timer

--- js/background.js
+++ js/background.js
@@ -3,40 +3,48 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         if (tab.url.includes("assessments/assignments")) {
             chrome.scripting.executeScript({
                 target: { tabId: tabId },
-                function: () => {
-                    let all = "ABCDE";
-
-                    setTimeout(() => {
-                        let question = document.getElementsByClassName("lrn_stimulus_content")[0].textContent;
-                        let answers = [];
-
-                        for (let answer of document.getElementsByClassName("lrn_mcqgroup")[0].children) {
-                            let text = answer.children[1].children[0].children[0].children[0].children[0].children[0].children[0].textContent;
-                            answers.push(`${all[answers.length]}. ${text}`);
-                        }
-
-                        prompt = `
-                        You will be given a question below, along with multiple choice answers. Your job is to output one singular letter, for the correct answer. 
-                        ######
-                        QUESTION:
-                        ${question}
-                        ####
-                        ANSWERS:
-                        ${answers.join("\n")}
-                        `;
-
-                        fetch('http://localhost:5000/api', {
-                            method: 'POST',
-                            headers: {'Content-Type': 'application/json'},
-                            body: JSON.stringify({prompt: prompt})
-                        })
-                        .then(response => response.json())
-                        .then(data => {
-                            console.log(data.res);
-                        });
-                    }, 5000);
-                },
+                func: call
             });
         }
     }
 });
+
+function call() {
+    document.addEventListener('keydown', function(event) {
+        if (event.key === '`') {
+            let all = "ABCDE";
+            let question = document.getElementsByClassName("lrn_stimulus_content")[0].textContent;
+            let answers = [];
+            
+            let elems = document.getElementsByClassName("lrn_mcqgroup");
+
+            for (let answer of elems[elems.length - 2].children) {
+                let text = answer.children[1].children[0].children[0].children[0].children[0].children[0].children[0].textContent;
+                answers.push(`${all[answers.length]}. ${text}`);
+            }
+
+            console.log(question)
+            console.log(answers);
+
+            let prompt = `
+            You will be given a question below, along with multiple choice answers. Your job is to output one singular letter, for the correct answer. 
+            ######
+            QUESTION:
+            ${question}
+            ####
+            ANSWERS:
+            ${answers.join("\n")}
+            `;
+
+            fetch('http://localhost:5000/api', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({prompt: prompt})
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.res);
+            });
+        }
+    });
+}
